b0d217f2577b5edea3bdd83adbf862d06e7e0e3b
Colby <thedarkness435@gmail.com>
2022-05-10 14:04:10 -0500

    Created Skill Object

--- ArknightsMaterialSelection/Operators/OperatorObject.cs
+++ ArknightsMaterialSelection/Operators/OperatorObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArknightsMaterialSelection.Operators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace ArknightsMaterialSelection
     {
         string? OperatorName { get; set; }
         string? OperatorFaction { get; set; }
+        List<SkillObject> Skills { get; set; }
     }
 }
---
+++ ArknightsMaterialSelection/Operators/SkillObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArknightsMaterialSelection.Operators
+{
+    internal class SkillObject
+    {
+        public string? SkillName { get; set; }
+        public string? SkillDescription { get; set; }
+    }
+}
